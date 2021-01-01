44026facff38d32987b5e1b3f0f8fc4bdb923a8d
edt-yxz-zzd <wuming_zher@zoho.com.cn>
2021-01-02 07:57:54 +0800

    ...

---
+++ lots/NOTE/stardict星际译王/dzip_src_2_9/crc32.c
@@ -0,0 +1,35 @@
+unsigned long crcval;
+unsigned long crctable[256];
+
+unsigned long crc_reflect(unsigned long x, int bits)
+{
+	int i;
+	unsigned long v = 0, b = 1 << (bits - 1);
+
+	for (i = 0; i < bits; i++)
+	{
+		if (x & 1) v += b;
+		x >>= 1; b >>= 1;
+	}
+	return v;
+}
+
+void crc_init(void)
+{
+	unsigned long crcpol = 0x04c11db7;
+	unsigned long i, j, k;
+
+	for (i = 0; i < 256; i++)
+	{
+		k = crc_reflect(i,8) << 24;
+		for (j = 0; j < 8; j++)
+			k = (k << 1) ^ ((k & 0x80000000)? crcpol : 0);
+		crctable[i] = crc_reflect(k,32);
+	}
+}
+
+void make_crc (unsigned char *ptr, int len)
+{
+	while (len--)
+		crcval = (crcval >> 8) ^ crctable[(crcval & 0xff) ^ *ptr++];
+}
---
+++ lots/NOTE/stardict星际译王/dzip_src_2_9/list.c
@@ -0,0 +1,42 @@
+#include "dzip.h"
+#include "dzipcon.h"
+
+void dzList (char *src)
+{
+	int i;
+	direntry_t *de;
+
+	if (!dzOpen(src, 0))
+		return;
+
+	printf("contents of %s, created using version %u.%u:\n",
+		src, maj_ver, min_ver);
+	de = directory;
+	for (i = 0; i < numfiles; i++, de++)
+	{
+		if (de->type == TYPE_DIR) 
+		{
+			printf(" %s\n", de->name);
+			continue;
+		}
+
+		if (de->pak && de->type != TYPE_PAK)
+		{
+			printf("  %-17s size: %-8u packed: %-8u\n", 
+				de->name, de->real, de->size);
+			continue;
+		}
+		printf(" %-18s size: %-8u packed: %-8u", 
+			de->name, de->real, de->size);
+		if (maj_ver != 1)
+			printf(" %02u.%02u.%4u %02u:%02u:%02u",
+				(de->date >> 16) & 0x1f,
+				((de->date >> 21) & 0x0f) + 1,
+				((de->date >> 25) & 0x7f) + 1980,
+				(de->date >> 11) & 0x1f,
+				(de->date >> 5) & 0x3f, (de->date & 0x1f) << 1);
+		printf("\n");
+	}
+	printf("\n");
+	dzClose();
+}
