db4ba2111cb59e72ad44885d5d87a702747f35d0
edt-yxz-zzd <wuming_zher@zoho.com.cn>
2021-02-01 07:18:35 +0800

    FC游戏

---
+++ txt/script/extract_xls_game_rom_names.py
@@ -0,0 +1,308 @@
+
+
+
+r"""
+=========
+e script/extract_xls_game_rom_names.py
+view /sdcard/0my_files/apk/游戏/街机/FC游戏列表(中英文).XLS.out.txt
+view others/app/app.txt
+  http://m.rsdown.cn/game/25587.html
+    FC游戏完整版(合集)全集打包1981个rom游戏.rar
+      231 MB
+=========
+
+#"""
+
+r"""
+
+,'4合1 (kevtris-Reset Based Four in One) 简体中文'
+    : 'kevtris-Reset.Based.Four.in.One.zip'
+,'三国志-群雄争霸 (Sango Fighter) 繁体中文'
+    : 'Sango.Fighter(UNL).zip'
+,'三国志2(sanguo2cn) 简体中文版'
+    : 'sanguo2cn.zip'
+,'三国志2-霸王的大陆 (简体中文)'
+    : 'sanguozhi2_chs.rar'
+,'三国志2-霸王的大陆 超级版 (简体中文)'
+    : 'bawangdedalu_chs.zip'
+,'三国志英杰传/吞食天地 (tstd) 简体中文'
+    : 'tstd.zip'
+,'上海大亨 (Shang Hai Tycoon) 简体中文版'
+    : 'Shang.Hai.Tycoon(C).zip'
+,'中国象棋 (Zhong Guo Xiang Qi) 繁体中文'
+    : 'Zhong.Guo.Xiang.Qi.zip'
+,'乱舞街头霸王3 (Turbo Street Fighter III) 简体中文版'
+    : 'Turbo.Street.Fighter.III.zip'
+,'人间兵器 (viper) 简体中文'
+    : 'viper(c).zip'
+,'仙剑奇侠传 (简体中文)'
+    : 'pal.zip'
+,'侠客情-荆轲刺秦王 (简体中文)'
+    : 'xiakeqing_chs.zip'
+,'假面忍者花丸 (简体中文)'
+    : 'Kamen.No.Ninja-Hanamaru(chs).zip'
+,'公路赛车/马路英雄/火箭车 (ROADSTRICKET) 简体中文'
+    : 'ROADSTRICKET.zip'
+,'动动脑2-国中英文 (Middle School English II) 简体中文'
+    : 'Middle.School.English.II(Sachen)(Unl).zip'
+,'包青天 (Bao Qing Tian) 简体中文'
+    : 'Bao.Qing.Tian(C).zip'
+,'匹诺曹的复苏 (简体中文)'
+    : 'Pinocchio_chs.zip'
+,'十项全能运动会 (Cecathlon) 繁体中文'
+    : 'Cecathlon(C).zip'
+,'双截龙 (Double Dragon) 简体中文版'
+    : 'Double.Dragon(Chs).zip'
+,'双截龙2 (Double Dragon 2) 简体中文'
+    : 'Double.Dragon.2(Chs).zip'
+,'双截龙3（Double Dragon 3) 简体中文版'
+    : 'Double.Dragon3(Chs).zip'
+,'口袋妖怪-金 (繁体中文)'
+    : 'koudaiyaoguaijin(cht).zip'
+,'古巴战士/革命英雄 (gbzscn) 简体中文'
+    : 'gbzscn.zip'
+,'台湾16张麻将 (16 Mahjang) 繁体中文'
+    : '16.Mahjang(need.correct.title)(Unl).zip'
+,'吞食天地2-诸葛孔明传 (tstd2c) 简体中文'
+    : 'tstd2c.zip'
+,'圣斗士星矢2 简体中文'
+    : 'Saint.Seiya-Ougon.Densetsu.Kanketsu.Hen(J)[T-Chinese].zip'
+,'圣火列传 (Shen Huo Le Zhuan) 简体中文'
+    : 'Shen.Huo.Le.Zhuan(C).zip'
+,'地道战 (繁体中文)'
+    : 'ddz.zip'
+,'坦克仔 (简体中文)'
+    : 'tankeza_chs.zip'
+,'坦克大战/打坦克 (简体中文)'
+    : 'Battle.City_chs.zip'
+,'基督山恩仇记 繁体中文'
+    : 'jdsecj.zip'
+,'外星战士 (Wai Xin Zhan Shi) 简体中文'
+    : 'Wai.Xin.Zhan.Shi.zip'
+,'大战略 (Daisenryaku) 简体中文版'
+    : 'Daisenryaku(chs).zip'
+,'大挑战!空中斗士 (Big Challenge! Dogfight Spirit) 英文 磁碟机版'
+    : 'Big.Challenge!Dogfight.Spirit.zip'
+,'大话西游 (gowest) 简体中文'
+    : 'gowest.zip'
+,'学习机学习卡 (Edu) 简体中文'
+    : 'Edu(C).zip'
+,'宠物小精灵 (红版) 简体中文'
+    : 'pockmonred.zip'
+,'导弹坦克 (m-tank) 简体中文'
+    : 'm-tank.zip'
+,'小霸王中英文学习卡 V4.0 (xiaobawang4) 简体中文'
+    : 'xiaobawang4.zip'
+,'小霸王系列学习软件 - 英语词霸 (yingyuciba) 简体中文'
+    : 'yingyuciba.zip'
+,'岳飞传 简体中文'
+    : 'yuefeizhuan_cht.zip'
+,'希特勒复活/希魔复活/暗杀希特勒 (简体中文)'
+    : 'xitelefuhuo_chs.zip'
+,'帝国战机 (Crisis Force) 简体中文版'
+    : 'Crisis.Force(J)[T+Chi].zip'
+,'帝国风暴 (Napoleon swar) 简体中文版'
+    : 'Napoleon.swar.zip'
+,'幽游白书-魔界最强列传 简体中文'
+    : 'youyou_c.zip'
+,'忍者神龟2 (TMNT2) 简体中文版'
+    : 'Super.Robin.Hood.zip'
+,'忍者龙剑传2 (Ninja Ryuukenden 2 - Ankoku No Jashin Ken) 简体中文'
+    : 'Ninja.Ryuukenden.2-Ankoku.No.Jashin.Ken(c).zip'
+,'忍者龙剑传3 (Ninja Ryukenden 3 - Yomi No Hakobune) 简体中文'
+    : 'Ninja.Ryukenden.3-Yomi.No.Hakobune(J)[T-Chinese]-S.zip'
+,'忍者龙剑传3 (Ninja Ryuukenden 3 - Yomi No Hakobune) 简体中文'
+    : 'Ninja.Ryuukenden.3-Yomi.No.Hakobune(c).zip'
+,'快打传说 (繁体中文)'
+    : 'street.finhter(cht).zip'
+,'快打旋风 (简体中文)'
+    : 'kuaidaxuanfeng_chs.zip'
+,'恶魔之剑/不动明王传 (简体中文)'
+    : 'emzj_chs.zip'
+,'成龙之龙 (clzlcn) 简体中文'
+    : 'clzlcn.zip'
+,'最终任务/空中魂斗罗 (Final Mission) 简体中文版'
+    : 'Final.Mission(J)[T-CH].zip'
+,'月宫桌球 (lunar pool) 简体中文'
+    : 'lunar.pool(c).zip'
+,'机器猫小叮当 (简体中文)'
+    : 'doraemon_cn.zip'
+,'杀虫大战 (Millipede) 简体中文'
+    : 'Millipede_chs.zip'
+,'泰坦尼克号 (繁体中文)'
+    : 'Titanic_cht.zip'
+,'洛克人2 (简体中文)'
+    : 'rockman2(chs).zip'
+,'洛克人6 (Rockman 6 - Shijou Saidai no Tatakai!!) 简体中文'
+    : 'Rockman.6-Shijou.Saidai.no.Tatakai!!(J)[T-Chinese].zip'
+,'火炎之纹章外传/圣火徽章外传 (简体中文版)'
+    : 'shenghuohuizhangwaizhuan.rar'
+,'烈火92 (Summer Carnival 92 - Recca) 简体中文版'
+    : 'Summer.Carnival.92-Recca(J)[T+Chi].zip'
+,'热血足球3 (简体中文)'
+    : 'rexuezuqiu3_chs.zip'
+,'爆笑三国 (繁体中文)'
+    : 'baoxiaosanguo_cht.zip'
+,'特库摩世界杯足球赛 (Tecmo World Cup) 简体中文'
+    : 'Tecmo.World.Cup_chs.zip'
+,'特救指令 (Tokkyuu Shirei - Solbrain) 简体中文'
+    : 'Tokkyuu.Shirei_chs.zip'
+,'生化危机 (简体中文)'
+    : 'Resident_chs.zip'
+,'甲A风云 繁体中文'
+    : 'jafy.zip'
+,'神风马里奥 (kamikaze) 简体中文'
+    : 'kamikaze.zip'
+,'神风马里奥2 (kamikaze2) 简体中文'
+    : 'kamikaze2.zip'
+,'神风马里奥3 (kamikaze3) 简体中文'
+    : 'kamikaze3.zip'
+,'第二次机器人大战 (srw2) 简体中文'
+    : 'srw2.zip'
+,'第四次机器人大战 (繁体中文)'
+    : 'd4cjqrdz.zip'
+,'绿色兵团 (Rush n Attack) 简体中文'
+    : 'Rush.n.Attack(U)[T-Chinese].zip'
+,'绿野仙踪 (繁体中文)'
+    : 'lyxz.zip'
+,'网球 (Tennis) 简体中文'
+    : 'TENNIS(cn).zip'
+,'耀奇的饼干中文/耀奇蛋糕中文 简体中文'
+    : 'Yoshi.No.Tamago(C).zip'
+,'脱狱 (简体中文)'
+    : 'Datsugoku_chs.zip'
+,'荆轲新传 (Gen Ke Le Zhuan) 简体中文'
+    : 'Gen.Ke.Le.Zhuan(C).zip'
+,'荒野大镖客 (简体中文)'
+    : 'gun.smoke(chs).zip'
+,'蝙蝠侠 (Batman) 简体中文版'
+    : 'Batman(U)[T+Chi].zip'
+,'赌神 (繁体中文)'
+    : 'dushen_cht.zip'
+,'赤影战士 (Kage) 简体中文'
+    : 'Kage(C).zip'
+,'超级中国拳2 (Linus Spacehead s Cosmic Crusade) 美版'
+    : 'Linus.Spacehead.s.Cosmic.Crusade(U).zip'
+,'超级中国拳美版/超级中国人美版 (Kung-Fu Heroes) 美版'
+    : 'Kung-Fu.Heroes(U).zip'
+,'超级战魂/魂斗罗7  (ZZZ_UNK_Super Contra 7) 简体中文'
+    : 'ZZZ_UNK_Super.Contra.7.zip'
+,'超级马里奥 (Super Mario Bros) 简体中文'
+    : 'Super.Mario.Bros(J).[T-Chinese].zip'
+,'超级魂斗罗 (scontra) 简体中文'
+    : 'scontra.zip'
+,'趣味方块 (Flipull) 简体中文'
+    : 'Flipull_chs.zip'
+,'轰天至尊 (ThunderWarrior) 简体中文'
+    : 'ThunderWarrior.zip'
+,'迷魂车 (Jovial Race) 简体中文'
+    : 'Jovial.Race(Clamshell).zip'
+,'金块中文/淘金者中文 (tjzcn) 简体中文'
+    : 'tjzcn.zip'
+,'锄大D (BIG2) 繁体中文版'
+    : 'BIG2.zip'
+,'隋唐演义 (SuiTang) 简体中文'
+    : 'SuiTang(C).zip'
+,'饿狼传说2 (Garou Densetsu 2) 繁体中文'
+    : 'Garou.Densetsu2.zip'
+,'饿狼传说特别版 (Garou Densetsu Special) 繁体中文'
+    : 'Garou.Densetsu.Special.zip'
+,'马戏团 (简体中文)'
+    : 'Circus.Charlie_chs .zip'
+,'马里奥医生 (Dr Mario) 简体中文'
+    : 'Dr.Mario(C).zip'
+,'高桥名人的冒险岛 (简体中文)'
+    : 'mxdcn.zip'
+,'高桥名人的冒险岛2 (简体中文)'
+    : 'gaoqiaomingren2_chs.zip'
+,'高桥名人的冒险岛3 (简体中文)'
+    : 'gaoqiaomingren3_chs.zip'
+,'魂斗罗 (简体中文)'
+    : 'Contra_chs.zip'
+,'魂斗罗2 (Super Contra) 简体中文'
+    : 'Super.Contra(Chs).zip'
+,'魔神法师 (moshenfashicn) 简体中文'
+    : 'moshenfashicn.zip'
+,'魔神英雄传中文/创世纪英雄中文 (wataru) 简体中文'
+    : 'wataru.zip'
+,'魔道士阴谋/超魔兽大战 (繁体中文) 磁碟机版'
+    : 'mdsym.zip'
+,'黑暗之蛊 (简体中文)'
+    : 'heianzhigu_cht.zip'
+,'龙珠Z - 超武斗传5 (Dragon Ball Z - Super Butoden 5) 简体中文'
+    : 'dbz5cn.zip'
+
+#"""
+
+
+
+
+
+
+ifnm = '/sdcard/0my_files/apk/游戏/街机/FC游戏列表(中英文).XLS'
+ofnm = f'{ifnm}.out.txt'
+
+import xlrd
+from pprint import pprint
+#bk = xlrd.open_workbook(ifnm)
+with xlrd.open_workbook(ifnm) as bk:
+    nms = bk.sheet_names()
+    #ls = bk.sheets()
+    assert nms == ['Mesky_Down_Address']
+    [shnm] = nms
+    sh = bk.sheet_by_name(shnm)
+    nr = sh.nrows
+    nc = sh.ncols
+    assert (nr, nc) == (1982, 2)
+    cn2rom = {}
+    dup = {}
+    for ri in range(nr):
+      row = []
+      for ci in range(nc):
+        ce = sh.cell(ri, ci)
+        #print(ce)
+        assert ce.ctype == 1 == xlrd.XL_CELL_TEXT
+        assert type(ce.value) is str
+        row.append(ce.value)
+      [游戏中文名, ROM压缩文件名] = row
+      """
+      text:'游戏名称'
+      text:'ROM 名称'
+      text:'100万美元-幻之帝王篇 (Maboroshi No Teiou Hen) 日版'
+      text:'100_Man_$_Kid-Maboroshi_No_Teiou_Hen(J).zip'
+      """
+      print(row)
+      if 游戏中文名 in cn2rom:
+        if 游戏中文名 not in dup:
+          dup[游戏中文名] = [cn2rom[游戏中文名]]
+        dup[游戏中文名].append(ROM压缩文件名)
+      cn2rom[游戏中文名] = ROM压缩文件名
+#if dup: pprint(dup)
+assert len(dup) == 1
+assert dup == {
+    '未来小子 (Metal Fighter) 美版':
+        ['Metal Fighter.zip'
+        ,'Metal.Fighter(Unl)(Joy.Van)(Asia).zip']
+    }
+cn2rom.update({
+    '未来小子 (Metal Fighter) ':
+        'Metal Fighter.zip'
+    ,'未来小子 (Metal Fighter) 美版':
+        'Metal.Fighter(Unl)(Joy.Van)(Asia).zip'
+    })
+assert len(cn2rom) == nr
+del cn2rom['游戏名称']
+assert len(cn2rom) == 1981
+
+
+
+cn2rom
+ofnm
+from pprint import pprint
+from seed.for_libs.for_pprint.pprint1 import pprint1
+with open(ofnm, 'x', encoding='utf8') as fout:
+    print('cn2rom = \\', file=fout)
+    #pprint(cn2rom, stream=fout)
+    pprint1(cn2rom, file=fout)
+
+
